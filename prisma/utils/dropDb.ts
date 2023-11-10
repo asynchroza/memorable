4e47fe5b125f034a4db7201b8aa0f52f0746508c
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-11-11 01:22:15 +0200

    Fix utility script for dropping database

--- prisma/utils/dropDb.ts
+++ prisma/utils/dropDb.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from '@prisma/client'
+
+process.env.DATABASE_URL="postgresql://dev:dev1234@localhost:5432/dev_db"
 const prisma = new PrismaClient()
 
-const dbName = "memorableDb"
-await prisma.$executeRawUnsafe(`DROP DATABASE IF EXISTS ${dbName};`);
+const dbName = "memorable_db"
+try {
+    await prisma.$executeRawUnsafe(`DROP DATABASE IF EXISTS ${dbName};`);
+    console.log(`Successfully deleted "${dbName}" database!`)
+} catch (e) {
+    console.error(e);
+}
