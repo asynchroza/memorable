c1f17df69121723ceab45065d160824189a902aa
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-12-01 21:39:23 +0200

    Add comment about how to use session wrapper

--- src/app/_components/utilities/ClientSideSessionWrapper.tsx
+++ src/app/_components/utilities/ClientSideSessionWrapper.tsx
@@ -3,9 +3,23 @@
 import { SessionProvider } from "next-auth/react";
 import { type Session } from "next-auth";
 
+/*
+  Use this wrapper when you want to access a user's session within a client side component.
+  This enables the use of `useSession` hook provided by `next-auth/react`.
+
+  --- example ---
+
+  const session = getServerAuthSession();
+
+  return (
+    <ClientSideSessionWrapper session={session}>
+      <MyClientSideComponent/>
+    </ClientSideSessionWrapper>
+  );
+*/
+
 export default function ClientSideSessionWrapper({children, session}: {children: React.ReactNode , session: Session | null}) {
   return (  
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     <SessionProvider session={session}>
         {children}
     </SessionProvider>
